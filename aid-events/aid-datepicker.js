1ca2f9048f2ad91ad96f794532f890c8ea42b2f6
Florian Bentele <fbentele@gmail.com>
2016-08-03 16:29:16 +0200

    added proper datepicker

--- aid-events/aid-datepicker.js
+++ aid-events/aid-datepicker.js
@@ -1,3 +1,8 @@
 jQuery( ".datepicker" ).datepicker({
-	dateFormat: "dd.mm.yy"
+	dateFormat: "dd.mm.yy",
+	nextText: "Nächster Monat",
+	prevText: "Vorheriger Monat",
+	dayNames: [ "Sonntag", "Montag", "Dienstag", "Mittwoch", "Donnerstag", "Freitag", "Samstag" ],
+	dayNamesMin: [ "So", "Mo", "Di", "Mi", "Do", "Fr", "Sa" ],
+	monthNames: [ "Januar", "Februar", "März", "April", "Mai", "Juni", "Juli", "August", "September", "Oktober", "November", "Dezember" ]
 });
